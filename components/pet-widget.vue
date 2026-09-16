d69bec5a7860d859da7cbbd17ffd7b7da50c6c0c
godfith <85066228+godfith@users.noreply.github.com>
2026-09-16 15:24:02 +0800

    fix: pet widget drop missing run frame so homepage compiles

--- components/pet-widget.vue
+++ components/pet-widget.vue
@@ -7,7 +7,7 @@
   </view>
 </template>
 <script>
-var LINES = ['要订场吗', '歇一会儿', '嘿嘿～', '发球！', '今天状态不错']
+var LINES = ['要订场吗', '歇一会儿', '嘿嘿～', '发球！']
 export default {
   data() {
     return {
@@ -23,8 +23,7 @@ export default {
         require('./pet-frame-sit.js'),
         require('./pet-frame-lie.js'),
         require('./pet-frame-jump.js'),
-        require('./pet-frame-stand.js'),
-        require('./pet-frame-run.js')
+        require('./pet-frame-stand.js')
       ]
     }
   },
